36d6eee0a831cd964d74ab3ef9233496ef2db377
Dana Simmons <dsanalog@gmail.com>
2019-09-14 22:46:25 -0400

    First commit

---
+++ multiproxy.js
@@ -0,0 +1,9 @@
+
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const URL = "http://multiproxy.org/cgi-bin/search-proxy.pl";
+
+document.querySelectorAll("center")[1].innerText.split('\n');
+return line.match(/\d+\.\d+\.\d+\.\d+\.:\d+/)
