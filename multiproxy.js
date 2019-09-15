ab93077d892a7db414fe2939aea1349c6bc005a4
Dana Simmons <dsanalog@gmail.com>
2019-09-15 08:19:28 -0400

    Can't get JSDOM to return data going to try vanilla request library

--- multiproxy.js
+++ multiproxy.js
@@ -5,5 +5,19 @@ const { JSDOM } = jsdom;
 
 const URL = "http://multiproxy.org/cgi-bin/search-proxy.pl";
 
-document.querySelectorAll("center")[1].innerText.split('\n');
-return line.match(/\d+\.\d+\.\d+\.\d+\.:\d+/)
+JSDOM.fromURL(URL)
+.then( dom => {
+  const body =  dom.window.document.querySelector("center");
+  console.log(body);
+  console.log("Got ",body.length," number fo charachters");
+
+  //document.querySelectorAll("center")[1].innerText.split('\n');
+
+  const lines = body.split('\n');
+
+  console.log("Parsing ", lines.length, " number of lines");
+
+  lines.filter(( line ) => {
+    return line.match(/\d+\.\d+\.\d+\.\d+\.:\d+/)
+  });
+});
